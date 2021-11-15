e8e4554cab4eafc6b2bbf61a78e1ab23be0695b4
Programmer Sajeeb <sajeeb.programmer@gmail.com>
2021-11-15 23:24:43 +0600

    some change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-const port = process.env.PORT || 5git000
+const port=process.env.PORT||5000
 
 
 // Middleware
@@ -13,7 +13,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hxa3e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
 
 
 async function run() {
